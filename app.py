0fc02de73215e7855a5b05c9157517201489132d
Rodion <59920979+Radeon590@users.noreply.github.com>
2023-04-24 11:05:21 +0300

    video streaming

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
+import re
 import flask
-from flask import Flask, request, redirect, url_for, send_from_directory, make_response, jsonify
+from flask import Flask, request, redirect, url_for, send_from_directory, Response
 from flask_cors import CORS
 from werkzeug.utils import secure_filename
 
@@ -37,6 +38,44 @@ def download_video(filename):
 def display_video(filename):
     # print('display_video filename: ' + filename)
     return redirect(url_for('static', filename='uploads/' + filename), code=301)
+
+
+def get_chunk(file_path, byte1=None, byte2=None):
+    file_size = os.stat(file_path).st_size
+    start = 0
+
+    if byte1 < file_size:
+        start = byte1
+    if byte2:
+        length = byte2 + 1 - byte1
+    else:
+        length = file_size - start
+
+    with open(file_path, 'rb') as f:
+        f.seek(start)
+        chunk = f.read(length)
+    return chunk, start, length, file_size
+
+
+@app.route('/video_stream/<string:filename>')
+def get_file(filename):
+    range_header = request.headers.get('Range', None)
+    byte1, byte2 = 0, None
+    if range_header:
+        match = re.search(r'(\d+)-(\d*)', range_header)
+        groups = match.groups()
+
+        if groups[0]:
+            byte1 = int(groups[0])
+        if groups[1]:
+            byte2 = int(groups[1])
+
+    chunk, start, length, file_size = get_chunk(os.path.join(UPLOAD_FOLDER, filename), byte1, byte2)
+    response = Response(chunk, 206, mimetype='video/mp4',
+                    content_type='video/mp4', direct_passthrough=True)
+    response.headers.add('Content-Range', 'bytes {0}-{1}/{2}'.format(start, start + length - 1, file_size))
+    response.headers.add('Accept-Ranges', 'bytes')
+    return response
 
 
 if __name__ == "__main__":
